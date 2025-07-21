71140568287ab5e1ffa1162edeadfdaa0c8fbd8d
123getu <kenzokwok69@gmail.com>
2025-07-21 19:15:49 +0700

    Add files via upload

---
+++ NewTest13.py
@@ -0,0 +1,106 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height): 
+        super().__init__()
+ 
+        self.image = transform.scale(image.load(player_image), (wight, height)) 
+        self.speed = player_speed
+ 
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite): #this is the place where we have to set up the key to make the racket move
+    def update_r(self): #function for making the right racket move
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+    def update_l(self): #function for making the right racket move
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+    #supposed to be update_l (add by yourself), pick another key to make the left racket move
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+timer = 0
+
+racket1 = Player('Rectangle.jpg', 30, 200, 4, 50, 150) 
+racket2 = Player('Rectangle.jpg', 520, 200, 4, 50, 150)
+ball = GameSprite('Ball.png', 200, 200, 4, 50, 50)
+times = 0
+font.init()
+font = font.Font(None, 75)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+timer = font.render(str(times), True, (180, 0, 0))
+
+signature = font.render('123getu', True, (255,0,255))
+speed_x = 3
+speed_y = 3
+frame = 0
+mixer.init()
+mixer.music.load('PlaceholderMusic.mp3')
+mixer.music.play()
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        window.blit(timer,(200,400))
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+            mixer.Sound('Hit.wav').play()
+        
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < -80:
+            finish = True
+            window.blit(lose1, (90, 200))
+            game_over = True
+            mixer.Sound('Win.wav').play()
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (90, 200))
+            game_over = True
+            mixer.Sound('Win.wav').play()
+        if frame == 60:
+            times += 1
+            frame = 0
+        else:
+            frame += 1
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        timer = font.render((f'timer: {times}'), True, (180, 0, 0))
+    else:
+        mixer.music.stop()
+    window.blit(signature,(0,0))
+    display.update()
+    clock.tick(FPS)
